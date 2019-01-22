f3d2a219d1b7ec031b71a65bf979a9bb02c409af
Caleb Nelson <calebnelson@dhcp-v034-110.mobile.uci.edu>
2019-01-22 13:08:04 -0800

    prettier

--- hw1/classify.py
+++ hw1/classify.py
@@ -1,18 +1,20 @@
 #!/bin/python
 
-def train_classifier(X, y, c = 1, solver = "liblinear"):
-	"""Train a classifier using the given training data.
 
-	Trains a logistic regression on the input data with default parameters.
-	"""
-	from sklearn.linear_model import LogisticRegression
-	cls = LogisticRegression(C=c, solver=solver)
-	cls.fit(X, y)
-	return cls
+def train_classifier(X, y, c=1, solver="liblinear"):
+    """Train a classifier using the given training data.
+
+    Trains a logistic regression on the input data with default parameters.
+    """
+    from sklearn.linear_model import LogisticRegression
+    cls = LogisticRegression(C=c, solver=solver)
+    cls.fit(X, y)
+    return cls
+
 
 def evaluate(X, yt, cls):
-	"""Evaluated a classifier on the given labeled data using accuracy."""
-	from sklearn import metrics
-	yp = cls.predict(X)
-	acc = metrics.accuracy_score(yt, yp)
-	print("  Accuracy", acc)
+    """Evaluated a classifier on the given labeled data using accuracy."""
+    from sklearn import metrics
+    yp = cls.predict(X)
+    acc = metrics.accuracy_score(yt, yp)
+    print("  Accuracy", acc)
